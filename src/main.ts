d0d5f8a54d2ec5c06fcb7b4bdbe7ef1eac040c90
JHasselbring <janmykhail.hasselbring@outlook.com>
2022-09-05 12:52:35 -0500

    adds progress-cli

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import { generateRandomCode } from "./generateRandomCode";
 import disk from "diskusage";
+const cliProgress = require('cli-progress');
+const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
 let initialDiskSpace: number = Number();
 let path: string = 'c:';
@@ -22,8 +24,8 @@ disk.check(path, function (err: string, info: any) {
     console.log(err);
   } else {
     initialDiskSpace = info.free;
-
-    console.log(`Starting @ "${dir}"`);
+    bar1.start(initialDiskSpace, 0);
+    // console.log(`Starting @ "${dir}"`);
     let content = generateRandomCode(30000000);
     while (1) {
       disk.check(path, function (err: string, info: any) {
@@ -33,13 +35,14 @@ disk.check(path, function (err: string, info: any) {
           console.log(err);
         } else {
           currentDiskSpace = info.free;
+          bar1.update(initialDiskSpace - currentDiskSpace);
         }
-        let prg1: number = (currentDiskSpace / initialDiskSpace);
-        let prg2: number = prg1 * 100;
-        let finalPrg: number = parseInt((100 - prg2).toFixed(0));
+        // let prg1: number = (currentDiskSpace / initialDiskSpace);
+        // let prg2: number = prg1 * 100;
+        // let finalPrg: number = parseInt((100 - prg2).toFixed(0));
 
-        console.clear();
-        console.log(finalPrg + '% complete');
+        // console.clear();
+        // console.log(finalPrg + '% complete');
 
         let name: string = generateRandomCode(32);
         try {
